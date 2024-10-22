5234d0c87521818713345b1b7e6c5ae71e577e63
AoRyu6 <ryu17615@gmail.com>
2024-10-22 16:12:00 +0900

    chore: eslintのpluginを追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
-import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import perfectionist from 'eslint-plugin-perfectionist';
 import svelte from 'eslint-plugin-svelte';
+import unusedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 
@@ -8,6 +10,7 @@ export default ts.config(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
+	perfectionist.configs['recommended-natural'],
 	prettier,
 	...svelte.configs['flat/prettier'],
 	{
@@ -25,6 +28,24 @@ export default ts.config(
 			parserOptions: {
 				parser: ts.parser
 			}
+		}
+	},
+	{
+		plugins: {
+			'unused-imports': unusedImports
+		},
+		rules: {
+			'@typescript-eslint/no-unused-vars': 'off',
+			'unused-imports/no-unused-imports': 'error',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+					vars: 'all',
+					varsIgnorePattern: '^_'
+				}
+			]
 		}
 	},
 	{
